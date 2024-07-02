8a4ef3ede9e555cd0bc8f10596ad78544b4db547
caiodx <43646613+caiodx@users.noreply.github.com>
2024-07-02 16:20:20 +0100

    Implementado acoes da pagina da configuracao

--- src/app/pages/config/config.page.ts
+++ src/app/pages/config/config.page.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton, IonCheckbox, IonList, IonListHeader, IonItem, IonLabel, IonRadio, IonRadioGroup, IonInput } from '@ionic/angular/standalone';
 import { GlobalConfigService } from 'src/app/services/global-config.service';
-
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.page.html',
   styleUrls: ['./config.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonInput, IonRadioGroup, IonCheckbox, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton,  IonList, IonListHeader, IonItem, IonLabel, IonRadio ]
 })
 export class ConfigPage implements OnInit {
 
-  constructor(private globalConfigService: GlobalConfigService) {}
+  localizacaoSelecionada: string = 'pegarLocalizacaoAtual'; // Opção inicial
+  public nomeCidade: string = '';
+
+  constructor(private globalConfigService: GlobalConfigService, public navCtrl: NavController) {}
 
   ngOnInit() {
+  }
+
+  voltarPaginaAnterior() {
+    this.navCtrl.pop()
+  }
+
+  ChangeLocationType(event: CustomEvent){    
+    this.localizacaoSelecionada = event.detail.value
+    console.log(this.localizacaoSelecionada)
+  }
+
+  Teste(event: CustomEvent){
+    console.log(event)
   }
 
 }
